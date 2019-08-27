5df206c5951d732b11d9bb6bf7abb7b3b271bcac
Volker Bergner <volker@bergner.one>
2019-08-27 14:31:27 +0100

    Oystercard has 0 default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,10 @@
 require "oystercard"
 
 describe Oystercard do
+  describe "#balance" do
+    it "has a default balance of 0 when intiliazed" do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
 end
