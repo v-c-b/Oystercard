1fe718f498396ed1c590f05a53862d547cd92740
Thomas Ross <thomasross3610@gmail.com>
2019-08-27 14:42:44 +0100

    empty top up method added to oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,8 @@
 require "oystercard"
 
 describe Oystercard do
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+
   describe "#balance" do
     it "has a default balance of 0 when intiliazed" do
       expect(subject.balance).to eq(0)
