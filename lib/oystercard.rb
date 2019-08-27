10cb6ae0c45d48503adb09f81bf12fa8aeb36219
Volker Bergner <volker@bergner.one>
2019-08-27 15:33:40 +0100

    Implemented deduct method for Oystercard class and minimum balance requirement

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = -3 # tests cannot handle posirive limits, needs to be 0 or negative
 
   attr_reader :balance
 
@@ -13,10 +14,19 @@ class Oystercard
     @balance += amount
   end
 
+  def deduct(amount)
+    below_min_limit?(amount)
+    @balance -= amount
+  end
+
   private
 
   def over_limit?(amount)
     raise "You have reached your top-up limit of #{MAXIMUM_BALANCE}." if @balance + amount > MAXIMUM_BALANCE
+  end
+
+  def below_min_limit?(amount)
+    raise "Failed - you attempted to go below limit of #{MINIMUM_BALANCE}." if @balance - amount < MINIMUM_BALANCE
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require "oystercard"
 
 describe Oystercard do
   it { is_expected.to respond_to(:top_up).with(1).argument }
+  it { is_expected.to respond_to(:deduct).with(1).argument }
 
   describe "#balance" do
     it "has a default balance of 0 when intiliazed" do
@@ -10,10 +11,19 @@ describe Oystercard do
     it "is incremented after top-up" do
       expect{ subject.top_up 1 }.to change { subject.balance }.by 1
     end
-    it "raises an error if user attempts to top up the balance above £90" do
+    it "raises an error if user attempts to top up the balance above max balance limit" do
         subject.top_up Oystercard::MAXIMUM_BALANCE
         expect { subject.top_up 1 }.to raise_error "You have reached your top-up limit of #{Oystercard::MAXIMUM_BALANCE}."
-      end
+    end
+    it "is reduced after deduct" do
+      subject.top_up 1
+      expect{ subject.deduct 1 }.to change { subject.balance }.by -1
+    end
+    it "raises an error if user attempts to deduct to below the minimum balance requirement" do
+        subject.deduct(-1*(Oystercard::MINIMUM_BALANCE)+subject.balance)
+        expect { subject.deduct 1 }.to raise_error "Failed - you attempted to go below limit of #{Oystercard::MINIMUM_BALANCE}."
+    end
+
     end
 
   end
