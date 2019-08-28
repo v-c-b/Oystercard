fa400b9144a7db77eb69bc2f07d86d89705aa2b7
Volker Bergner <volker@bergner.one>
2019-08-28 15:00:42 +0100

    Added Station class with zone and name attributes implemented

---
+++ lib/station.rb
@@ -0,0 +1,10 @@
+class Station
+attr_reader :name, :zone
+
+def initialize(name = "Central Station", zone = 1 )
+  @name = name
+  @zone = zone
+end
+
+
+end
---
+++ spec/station_spec.rb
@@ -0,0 +1,24 @@
+require "station"
+
+describe Station do
+
+  describe "#exposes" do
+    it "name" do
+     is_expected.to respond_to(:name)
+    end
+    it "zone" do
+      is_expected.to respond_to(:zone)
+    end
+  end
+
+  describe "#default behaviour" do
+    it "has default name Central Station" do
+    expect(subject.name).to eq("Central Station")
+    end
+    it "has default zone of 1" do
+    expect(subject.zone).to eq(1)
+    end
+  end
+
+
+end
